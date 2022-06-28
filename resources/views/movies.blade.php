fba185f1cbc19acdc186c3cf91178a9171a572a8
N4kka <sarlinicola7@gmail.com>
2022-06-28 17:33:54 +0200

    bonus done: stylish page updated

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,13 +1,19 @@
-<h1>pagina con film</h1>
+@extends('layouts/app')
 
-<ul>
-    @foreach ($movies as $item)
-        <li>
-            <h1>{{ $item->title }}</h1>
-            <h2>{{ $item->original_title }}</h2>
-            <p>{{ $item->nationality }}</p>
-            <p>{{ $item->date }}</p>
-            <p>{{ $item->vote }}</p>
-        </li>
-    @endforeach
-</ul>
+@section('main-content')
+    <h1 class="header-title">Movies List:</h1>
+
+    <ul>
+        <div class="single-movie">
+            @foreach ($movies as $item)
+                <li>
+                    <h2>{{ $item->title }}</h2>
+                    <h4>{{ $item->original_title }}</h4>
+                    <p>{{ $item->nationality }}</p>
+                    <p class="date">{{ $item->date }}</p>
+                    <p>{{ $item->vote }}</p>
+                </li>
+            @endforeach
+        </div>
+    </ul>
+@endsection
